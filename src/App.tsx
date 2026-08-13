21ad06892ab6b8b20a0e1fe05e118f64701245d7
Nimira43 <n.rabone72@aol.com>
2026-08-13 05:38:24 +0100

    Project created.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className='p-20 bg-amber-400'>
+      <h1 className='text-8xl logo-font text-red-900'>
+        Bardiya
+      </h1>
+      <p>Event Manager</p>
+    </div>
+  )
+}
+
+export default App
