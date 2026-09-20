4df5c1bed883b53bb9899e80000bb2f9360cebd8
Nimira43 <n.rabone72@aol.com>
2026-09-20 09:49:01 +0100

    Updated theme and setting up Redux.

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './app/layout/globals.css'
 import App from './app/layout/App'
+import { Provider } from 'react-redux'
+import { store } from './lib/stores/store'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
